25e84c7235aa2023cd9f4e50b4f4ab49ea7cc466
dima tkachenko <dtkachenko06@gmail.com>
2023-06-02 14:14:32 +0300

    Project 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var subKeyNames = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+            string[] valueNames = subKeyNames.GetValueNames();
+            foreach (var item in valueNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
