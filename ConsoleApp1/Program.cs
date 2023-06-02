c5654363896f546f00086b8b65d6ce26052fb883
dima tkachenko <dtkachenko06@gmail.com>
2023-06-02 14:47:54 +0300

    Add get Value

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,10 +8,14 @@ namespace ConsoleApp1
         {
             var subKeyNames = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
 
+            
             string[] valueNames = subKeyNames.GetValueNames();
+
             foreach (var item in valueNames)
             {
                 Console.WriteLine(item);
+                string value = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", item, null);
+                Console.WriteLine(value);
             }
         }
     }
